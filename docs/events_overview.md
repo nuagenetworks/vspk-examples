e5eb71fa3dae585955e6d5999857a03a47dec81a
Philippe Dellaert <philippe@dellaert.org>
2016-01-26 10:07:06 +0100

    Adding events_overview and updating fip_overview

--- docs/events_overview.md
+++ docs/events_overview.md
@@ -1,24 +1,24 @@
-fip_overview.py
-===============
-fip_overview produces a table with information on all VMs with a FIP attached. Output can also be given in JSON format.
+events_overview.py
+==================
+event_overview produces a table with information on the events for each enterprise the user has access to. Output can also be given in JSON format.
 
 ### Author ###
 Philippe Dellaert <philippe.dellaert@nuagenetworks.net>
 
 ### Version history ###
 2016-01-24 - 1.0
-2016-01-26 - 1.1 - JSON output supported
 
 ### Usage ### 
-    usage: fip_overview.py [-h] [-d] [-j] [-l LOGFILE] -E NUAGE_ENTERPRISE -H
-                           NUAGE_HOST [-P NUAGE_PORT] [-p NUAGE_PASSWORD] -u
-                           NUAGE_USERNAME [-S] [-v]
+    usage: events_overview.py [-h] [-d] [-e] [-j] [-l LOGFILE] -E NUAGE_ENTERPRISE
+                              -H NUAGE_HOST [-P NUAGE_PORT] [-p NUAGE_PASSWORD] -u
+                              NUAGE_USERNAME [-S] [-t TIME_DIFFERENCE] [-v]
 
-    Tool to list all FIPs attached to the VMs to which the user has access to.
+    Tool to list all events on the enterpises to which the user has access to.
 
     optional arguments:
       -h, --help            show this help message and exit
       -d, --debug           Enable debug output
+      -e, --extended        Enable extended output
       -j, --json            Print as JSON, not as a table
       -l LOGFILE, --log-file LOGFILE
                             File to log to (default = stdout)
@@ -39,11 +39,26 @@ Philippe Dellaert <philippe.dellaert@nuagenetworks.net>
                             VSD/SDK host
       -S, --disable-SSL-certificate-verification
                             Disable SSL certificate verification on connect
+      -t TIME_DIFFERENCE, --time TIME_DIFFERENCE
+                            Indication of how far back in the past the events list
+                            should go. Can be set in seconds, minutes (add m),
+                            hours (add h) or days (add d) (examples: 60, 60m, 60h
+                            or 60d, default is 3600 seconds)
       -v, --verbose         Enable verbose output
 
 ### Example ###
-    python fip_overview.py -E csp -H 10.167.43.64 -P 443 -p csproot -u csproot -S
-    
+#### Basic table output ####
+    python event_overview.py -E csp -H 10.167.43.64 -P 443 -p csproot -u csproot -S
+
+#### Extended table output ####
+    python event_overview.py -E csp -H 10.167.43.64 -P 443 -p csproot -u csproot -S -e
+
+#### Basic JSON output ####
+    python event_overview.py -E csp -H 10.167.43.64 -P 443 -p csproot -u csproot -S -j
+
+#### Extended JSON output ####
+    python event_overview.py -E csp -H 10.167.43.64 -P 443 -p csproot -u csproot -S -e -j
+
 ### Requirements ###
 * Nuage VSPK/VSDK (3.2+)
 * PrettyTables (pip install prettytables)
--- events_overview.py
+++ events_overview.py
@@ -1,31 +1,43 @@
 # -*- coding: utf-8 -*-
 """
-fip_overview produces a table with information on all VMs with a FIP attached. Output can also be given in JSON format.
+event_overview produces a table with information on the events for each enterprise the user has access to. Output can also be given in JSON format.
 
 --- Author ---
 Philippe Dellaert <philippe.dellaert@nuagenetworks.net>
 
 --- Version history ---
-2016-01-24 - 1.0
-2016-01-26 - 1.1 - JSON output supported
+2016-01-26 - 1.0
 
 --- Usage --- 
-run 'python fip_overview.py -h' for an overview
+run 'python event_overview.py -h' for an overview
 
 --- Documentation ---
-http://github.com/nuagenetworks/vspk-examples/blob/master/docs/fip_overview.md
+http://github.com/nuagenetworks/vspk-examples/blob/master/docs/events_overview.md
 
 --- Example ---
-python fip_overview.py -E csp -H 10.167.43.64 -P 443 -p csproot -u csproot -S
+---- Basic table output ----
+python event_overview.py -E csp -H 10.167.43.64 -P 443 -p csproot -u csproot -S
+
+---- Extended table output ----
+python event_overview.py -E csp -H 10.167.43.64 -P 443 -p csproot -u csproot -S -e
+
+---- Basic JSON output ----
+python event_overview.py -E csp -H 10.167.43.64 -P 443 -p csproot -u csproot -S -j
+
+---- Extended JSON output ----
+python event_overview.py -E csp -H 10.167.43.64 -P 443 -p csproot -u csproot -S -e -j
 
 """
 
 import argparse
+import datetime
 import getpass
 import json
 import logging
 import os.path
+import re
 import requests
+import time
 
 from prettytable import PrettyTable
 
@@ -39,8 +51,9 @@ def get_args():
     Supports the command-line arguments listed below.
     """
 
-    parser = argparse.ArgumentParser(description="Tool to list all FIPs attached to the VMs to which the user has access to.")
+    parser = argparse.ArgumentParser(description="Tool to list all events on the enterpises to which the user has access to.")
     parser.add_argument('-d', '--debug', required=False, help='Enable debug output', dest='debug', action='store_true')
+    parser.add_argument('-e', '--extended', required=False, help='Enable extended output', dest='extended', action='store_true')
     parser.add_argument('-j', '--json', required=False, help='Print as JSON, not as a table', dest='json_output', action='store_true')
     parser.add_argument('-l', '--log-file', nargs=1, required=False, help='File to log to (default = stdout)', dest='logfile', type=str)
     parser.add_argument('-E', '--nuage-enterprise', nargs=1, required=True, help='The enterprise with which to connect to the Nuage VSD/SDK host', dest='nuage_enterprise', type=str)
@@ -49,6 +62,7 @@ def get_args():
     parser.add_argument('-p', '--nuage-password', nargs=1, required=False, help='The password with which to connect to the Nuage VSD/SDK host. If not specified, the user is prompted at runtime for a password', dest='nuage_password', type=str)
     parser.add_argument('-u', '--nuage-user', nargs=1, required=True, help='The username with which to connect to the Nuage VSD/SDK host', dest='nuage_username', type=str)
     parser.add_argument('-S', '--disable-SSL-certificate-verification', required=False, help='Disable SSL certificate verification on connect', dest='nosslcheck', action='store_true')
+    parser.add_argument('-t', '--time', nargs=1, required=False, help='Indication of how far back in the past the events list should go. Can be set in seconds, minutes (add m), hours (add h) or days (add d) (examples: 60, 60m, 60h or 60d, default is 3600 seconds)', dest='time_difference', type=str, default=['3600'])
     parser.add_argument('-v', '--verbose', required=False, help='Enable verbose output', dest='verbose', action='store_true')
     args = parser.parse_args()
     return args
@@ -61,6 +75,7 @@ def main():
     # Handling arguments
     args                = get_args()
     debug               = args.debug
+    extended            = args.extended
     json_output         = args.json_output
     log_file            = None
     if args.logfile:
@@ -73,6 +88,7 @@ def main():
         nuage_password  = args.nuage_password[0]
     nuage_username      = args.nuage_username[0]
     nosslcheck          = args.nosslcheck
+    time_difference     = args.time_difference[0]
     verbose             = args.verbose
 
     # Logging settings
@@ -85,6 +101,26 @@ def main():
 
     logging.basicConfig(filename=log_file, format='%(asctime)s %(levelname)s %(message)s', level=log_level)
     logger = logging.getLogger(__name__)
+
+    # Validating time_difference input
+    time_check = re.compile('^([0-9]+)([m|h|d]?)$')
+    time_matches = time_check.match(time_difference)
+    if time_matches is None:
+        logger.critical('The time indication %s is an invalid value, exiting' % time_difference)
+        return 1
+
+    time_diff = int(float(time_matches.group(1)))
+    if time_matches.group(2) == 'm':
+        logger.debug('Found m in the time difference, multiplying integer by 60')
+        time_diff *= 60
+    elif time_matches.group(2) == 'h':
+        logger.debug('Found h in the time difference, multiplying integer by 3600')
+        time_diff *= 3600
+    elif time_matches.group(2) == 'd':
+        logger.debug('Found d in the time difference, multiplying integer by 86400')
+        time_diff *= 86400
+
+    logger.debug('Time difference set to %s seconds' % time_diff)
 
     # Disabling SSL verification if set
     ssl_context = None
@@ -115,37 +151,46 @@ def main():
         return 1
 
     if json_output:
-        logger.debug('Setting up json output')
+        logger.debug('JSON output enabled, not setting up an output table')
         json_object = []
-    else:
+    elif extended:
+        logger.debug('Setting up extended output table')
+        pt = PrettyTable(['Enterprise','Timestamp','Date/Time','Type','Entity', 'Entity parent', 'Extended info'])
+    else:    
         logger.debug('Setting up basic output table')
-        pt = PrettyTable(['Enterprise','Domain','VM Name','VM IP','VM MAC','FIP'])
+        pt = PrettyTable(['Enterprise','Timestamp','Date/Time','Type','Entity', 'Entity parent'])
 
-    logger.debug('Getting FIPs for the logged in user.')
-    for nc_fip in nc.user.floating_ips.get():
-        logger.debug('Found FIP with IP %s' % nc_fip.address)
-        nc_vport = nc_fip.vports.get()[0]
-        nc_interface = nc_vport.vm_interfaces.get()[0]
-        nc_vm = nc_vport.vms.get()[0]
+    unix_check_time = time.time() - time_diff
+    logger.debug('Gathering all events from after UNIX timestamp %s' % unix_check_time)
 
-        if json_output:
-            json_dict = {
-            'Enterprise': nc_vm.enterprise_name, 
-            'Domain': nc_interface.domain_name, 
-            'VM Name': nc_vm.name, 
-            'VM IP': nc_interface.ip_address, 
-            'VM MAC': nc_interface.mac, 
-            'FIP': nc_fip.address
-            }
-            json_object.append(json_dict)
-        else:
-            logger.debug('Add row: %s, %s, %s, %s, %s, %s' % (nc_vm.enterprise_name, nc_interface.domain_name, nc_vm.name, nc_interface.ip_address, nc_interface.mac, nc_fip.address))
-            pt.add_row([nc_vm.enterprise_name, nc_interface.domain_name, nc_vm.name, nc_interface.ip_address, nc_interface.mac, nc_fip.address])
+    for ent in nc.user.enterprises.get():
+        logger.debug('Gathering events for enterprise %s' % ent.name)
+        for event in ent.event_logs.get(filter="eventReceivedTime >= '%s'" % int(unix_check_time*1000)):
+            logger.debug('Found event of type %s with timestamp %s' % (event.type, event.event_received_time))
+            clean_time = datetime.datetime.fromtimestamp(int(event.event_received_time/1000)).strftime('%Y-%m-%d %H:%M:%S')
 
+            if json_output:
+                json_dict = {
+                'Enterprise': ent.name,
+                'Timestamp': event.event_received_time,
+                'Date/Time': clean_time,
+                'Type': event.type,
+                'Entity': event.entity_type,
+                'Entity parent': event.entity_parent_type
+                }
+                if extended:
+                    json_dict['Extended info'] = event.entities
+                json_object.append(json_dict)
+            elif extended:
+                pt.add_row([ent.name, event.event_received_time, clean_time, event.type, event.entity_type, event.entity_parent_type, json.dumps(event.entities)])
+            else:
+                pt.add_row([ent.name, event.event_received_time, clean_time, event.type, event.entity_type, event.entity_parent_type])
+
+    logger.debug('Printing output')
     if json_output:
         print json.dumps(json_object, indent=4)
     else:
-        print pt
+        print pt.get_string(sortby='Timestamp')
 
     return 0
 
