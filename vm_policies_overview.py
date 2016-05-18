543c5debda51c8757ca2a68aae87ced7290608e2
Philippe Dellaert <philippe.dellaert@nuagenetworks.net>
2016-05-18 17:33:33 +0200

    Output cleanup

--- vm_policies_overview.py
+++ vm_policies_overview.py
@@ -3,6 +3,12 @@
 vm_policies_overview.py is a tool to display the policies that are applied
  to one or more VMs.
 
+The script will try and be intelligent and change the Ether type and Protocols
+ to human readable text.
+
+The script will not touch null or None values, because there is a difference
+ between None and * for instance
+
 --- Author ---
 Philippe Dellaert <philippe.dellaert@nuagenetworks.net>
 
@@ -14,6 +20,7 @@ Philippe Dellaert <philippe.dellaert@nuagenetworks.net>
 2016-05-18 - 0.5.0 - Fix for fetching data and log output
 2016-05-18 - 0.5.1 - Fix for fetching fetcher
 2016-05-18 - 0.5.2 - Fix for fetching object instead of array
+2016-05-18 - 0.6.0 - Output cleanup
 
  --- Usage ---
 run 'vm_policies_overview.py -h' for an overview
@@ -27,6 +34,193 @@ import requests
 
 from prettytable import PrettyTable
 from vspk import v4_0 as vsdk
+
+ether_types = {
+    '0x0800': 'IPv4',
+    '0x0806': 'ARP',
+    '0x0842': 'Wake-on-LAN',
+    '0x22F3': 'IETF TRILL',
+    '0x6003': 'DECnet Phase IV',
+    '0x8035': 'RARP',
+    '0x809B': 'AppleTalk ',
+    '0x80F3': 'AARP',
+    '0x8100': '802.1Q and 802.1aq',
+    '0x8137': 'IPX',
+    '0x8204': 'QNX Qnet',
+    '0x86DD': 'IPv6',
+    '0x8808': 'Ethernet flow control',
+    '0x8819': 'CobraNet',
+    '0x8847': 'MPLS unicast',
+    '0x8848': 'MPLS multicast',
+    '0x8863': 'PPPoE Discovery Stage',
+    '0x8864': 'PPPoE Session Stage',
+    '0x8870': 'Jumbo Frames (proposed)',
+    '0x887B': 'HomePlug 1.0 MME',
+    '0x888E': 'EAP over LAN (IEEE 802.1X)',
+    '0x8892': 'PROFINET Protocol',
+    '0x889A': 'HyperSCSI (SCSI over Ethernet)',
+    '0x88A2': 'ATA over Ethernet',
+    '0x88A4': 'EtherCAT Protocol',
+    '0x88A8': 'Provider Bridging (IEEE 802.1ad) ',
+    '0x88AB': 'Ethernet Powerlink',
+    '0x88CC': 'LLDP',
+    '0x88CD': 'SERCOS III',
+    '0x88E1': 'HomePlug AV MME',
+    '0x88E3': 'Media Redundancy Protocol (IEC62439-2)',
+    '0x88E5': 'MAC security (IEEE 802.1AE)',
+    '0x88E7': 'Provider Backbone Bridges (PBB) (IEEE 802.1ah)',
+    '0x88F7': 'Precision Time Protocol (PTP) over Ethernet (IEEE 1588)',
+    '0x8902': 'IEEE 802.1ag Connectivity Fault Management (CFM) Protocol ',
+    '0x8906': 'FCoE',
+    '0x8914': 'FCoE Initialization Protocol',
+    '0x8915': 'RoCE',
+    '0x891D': 'TTE',
+    '0x892F': 'HSR',
+    '0x9000': 'Ethernet Configuration Testing Protocol'
+}
+
+protocols = {
+    '0': 'HOPOPT',
+    '1': 'ICMP',
+    '2': 'IGMP',
+    '3': 'GGP',
+    '4': 'IPv4',
+    '5': 'ST',
+    '6': 'TCP',
+    '7': 'CBT',
+    '8': 'EGP',
+    '9': 'IGP',
+    '10': 'BBN-RCC-MON',
+    '11': 'NVP-II',
+    '12': 'PUP',
+    '13': 'ARGUS',
+    '14': 'EMCON',
+    '15': 'XNET',
+    '16': 'CHAOS',
+    '17': 'UDP',
+    '18': 'MUX',
+    '19': 'DCN-MEAS',
+    '20': 'HMP',
+    '21': 'PRM',
+    '22': 'XNS-IDP',
+    '23': 'TRUNK-1',
+    '24': 'TRUNK-2',
+    '25': 'LEAF-1',
+    '26': 'LEAF-2',
+    '27': 'RDP',
+    '28': 'IRTP',
+    '29': 'ISO-TP4',
+    '30': 'NETBLT',
+    '31': 'MFE-NSP',
+    '32': 'MERIT-INP',
+    '33': 'DCCP',
+    '34': '3PC',
+    '35': 'IDPR',
+    '36': 'XTP',
+    '37': 'DDP',
+    '38': 'IDPR-CMTP',
+    '39': 'TP++',
+    '40': 'IL',
+    '41': 'IPv6',
+    '42': 'SDRP',
+    '43': 'IPv6-Route',
+    '44': 'IPv6-Frag',
+    '45': 'IDRP',
+    '46': 'RSVP',
+    '47': 'GRE',
+    '48': 'DSR',
+    '49': 'BNA',
+    '50': 'ESP',
+    '51': 'AH',
+    '52': 'I-NLSP',
+    '53': 'SWIPE',
+    '54': 'NARP',
+    '55': 'MOBILE',
+    '56': 'TLSP',
+    '57': 'SKIP',
+    '58': 'IPv6-ICMP',
+    '59': 'IPv6-NoNxt',
+    '60': 'IPv6-Opts',
+    '62': 'CFTP',
+    '64': 'SAT-EXPAK',
+    '65': 'KRYPTOLAN',
+    '66': 'RVD',
+    '67': 'IPPC',
+    '69': 'SAT-MON',
+    '70': 'VISA',
+    '71': 'IPCV',
+    '72': 'CPNX',
+    '73': 'CPHB',
+    '74': 'WSN',
+    '75': 'PVP',
+    '76': 'BR-SAT-MON',
+    '77': 'SUN-ND',
+    '78': 'WB-MON',
+    '79': 'WB-EXPAK',
+    '80': 'ISO-IP',
+    '81': 'VMTP',
+    '82': 'SECURE-VMTP',
+    '83': 'VINES',
+    '84': 'TTP',
+    '84': 'IPTM',
+    '85': 'NSFNET-IGP',
+    '86': 'DGP',
+    '87': 'TCF',
+    '88': 'EIGRP',
+    '89': 'OSPFIGP',
+    '90': 'Sprite-RPC',
+    '91': 'LARP',
+    '92': 'MTP',
+    '93': 'AX.25',
+    '94': 'IPIP',
+    '95': 'MICP',
+    '96': 'SCC-SP',
+    '97': 'ETHERIP',
+    '98': 'ENCAP',
+    '100': 'GMTP',
+    '101': 'IFMP',
+    '102': 'PNNI',
+    '103': 'PIM',
+    '104': 'ARIS',
+    '105': 'SCPS',
+    '106': 'QNX',
+    '107': 'A/N',
+    '108': 'IPComp',
+    '109': 'SNP',
+    '110': 'Compaq-Peer',
+    '111': 'IPX-in-IP',
+    '112': 'VRRP',
+    '113': 'PGM',
+    '115': 'L2TP',
+    '116': 'DDX',
+    '117': 'IATP',
+    '118': 'STP',
+    '119': 'SRP',
+    '120': 'UTI',
+    '121': 'SMP',
+    '122': 'SM',
+    '123': 'PTP',
+    '124': 'ISIS over IPv4',
+    '125': 'FIRE',
+    '126': 'CRTP',
+    '127': 'CRUDP',
+    '128': 'SSCOPMCE',
+    '129': 'IPLT',
+    '130': 'SPS',
+    '131': 'PIPE',
+    '132': 'SCTP',
+    '133': 'FC',
+    '134': 'RSVP-E2E-IGNORE',
+    '135': 'Mobility Header'
+    '136': 'UDPLite',
+    '137': 'MPLS-in-IP',
+    '138': 'manet',
+    '139': 'HIP',
+    '140': 'Shim6',
+    '141': 'WESP',
+    '142': 'ROHC',
+    '255': 'Reserved'
+}
 
 
 def get_args():
@@ -334,6 +528,24 @@ def main():
                 output['Destination port'] = acl_rule.destination_port
                 output['DSCP'] = acl_rule.dscp
                 output['Action'] = acl_rule.action
+
+            # Output cleanup
+            if output['Ether Type'] in ether_types.keys():
+                output['Ether Type'] = ether_types[output['Ether Type']]
+
+            if output['Protocol'] in protocols.keys():
+                output['Protocol'] = protocols[output['Protocol']]
+
+            if output['Source type'] == 'ANY':
+                output['Source name'] = '*'
+
+            if output['Destination type'] == 'ANY':
+                output['Source name'] = '*'
+
+            # Cleanup None values
+            for key in output.keys():
+                if output[key] is None:
+                    output[key] = ''
 
             logger.debug('Saving output to output object')
             if configuration['json_output']:
