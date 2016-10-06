f86f376114e58b8363cb76ae3634f719cf723cec
root <mmkashin@gmail.com>
2016-10-06 14:50:23 -0400

    Variable name changes and failure handling

--- docs/fip_to_gateway.md
+++ docs/fip_to_gateway.md
@@ -7,16 +7,17 @@ Michael Kashin <michael.kashin@nokia.com>
 
 ### Version history ###
 2016-10-06 - 1.0 - First version
+2016-10-08 - 1.1 - Variable name changes and failure handling
 
 ### Usage ###
 usage: fip_to_gateway.py [-h] [-d] [-l LOGFILE] -E NUAGE_ENTERPRISE -H
                          NUAGE_HOST [-P NUAGE_PORT] [-p NUAGE_PASSWORD] -u
                          NUAGE_USERNAME [-S] [-v] --fip FIP_NET --address
                          UPLINK_ADDR --mask UPLINK_MASK --gw UPLINK_GW --ip
-                         UPLINK_IP --mac UPLINK_MAC --vrs VRS_IP --port
-                         VRS_PORT --vlan VRS_VLAN
+                         UPLINK_IP --mac UPLINK_MAC --vsg GW_NAME --port
+                         GW_PORT --vlan GW_VLAN
 
-Tool to create an uplink subnet for FIP access via VSR gateway.
+Tool to create an uplink subnet for FIP access via VSG/VRS-G gateway.
 
 optional arguments:
   -h, --help            show this help message and exit
@@ -48,20 +49,21 @@ optional arguments:
   --gw UPLINK_GW        Uplink network gateway
   --ip UPLINK_IP        Uplink interface IP
   --mac UPLINK_MAC      Uplink interface MAC
-  --vrs VRS_IP          VRS Gateway IP address
-  --port VRS_PORT       VRS Gateway Network Interface Name
-  --vlan VRS_VLAN       VRS Gateway Network Interface Vlan ID
+  --vsg GW_NAME         VSG/VRS-G name as it appears in your infrastructure
+                        (defaults to IP if you have not changed it)
+  --port GW_PORT        VSG/VRS-G Network Interface Name
+  --vlan GW_VLAN        VSG/VRS-G Network Interface Vlan ID
 
 ### Example ###
 python fip_to_gateway.py -E csp -H 10.6.132.43 -p csproot -u csproot -S \
        --address 10.6.132.160 --mask 255.255.255.224 --gw 10.6.132.190 \
        --ip 10.6.132.161 --mac f4:cc:55:e0:14:00 --fip 10.6.132.192 \
-       --vrs 10.6.132.47 --port eth0 --vlan 205
+       --vsg 10.6.132.47 --port eth0 --vlan 205
 
 ### Requirements ###
-* Nuage VSPK/VSDK (3.2+)
+* Nuage VSPK/VSDK 4.0
 
 ### Nuage Requirements ###
-* VSG/VRS-G gateway should exist and the vlan MUST NOT be configured
+* VSG/VRS-G gateway should exist and the vlan MUST NOT be configured on the port
 * Floating IP subnet should be configured
 
--- fip_to_gateway.py
+++ fip_to_gateway.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-fip_to_gateway creates an uplink subnet on VRS/VRS-G gateway to access FIP subnet inside Shared Resource Domain
+fip_to_gateway creates an uplink subnet on VSG/VRS-G gateway to access FIP subnet inside Shared Resource Domain
 
 --- Author ---
 Michael Kashin <michael.kashin@nokia.com>
 
 --- Version history ---
 2016-10-06 - 1.0 - First version
+2016-10-08 - 1.1 - Variable name changes and failure handling
 
 --- Usage ---
 run 'python fip_to_gateway.py -h' for an overview
@@ -18,7 +19,7 @@ http://github.com/nuagenetworks/vspk-examples/blob/master/docs/fip_to_gateway.md
 python fip_to_gateway.py -E csp -H 10.6.132.43 -p csproot -u csproot -S \
        --address 10.6.132.160 --mask 255.255.255.224 --gw 10.6.132.190 \
        --ip 10.6.132.161 --mac f4:cc:55:e0:14:00 --fip 10.6.132.192 \
-       --vrs 10.6.132.47 --port eth0 --vlan 205
+       --vsg 10.6.132.47 --port eth0 --vlan 205
 """
 
 import argparse
@@ -35,7 +36,7 @@ def get_args():
     Supports the command-line arguments listed below.
     """
 
-    parser = argparse.ArgumentParser(description="Tool to create an uplink subnet for FIP access via VSR gateway.")
+    parser = argparse.ArgumentParser(description="Tool to create an uplink subnet for FIP access via VSG/VRS-G gateway.")
     parser.add_argument('-d', '--debug', required=False, help='Enable debug output', dest='debug', action='store_true')
     parser.add_argument('-l', '--log-file', required=False, help='File to log to (default = stdout)', dest='logfile', type=str)
     parser.add_argument('-E', '--nuage-enterprise', required=True, help='The enterprise with which to connect to the Nuage VSD/SDK host', dest='nuage_enterprise', type=str)
@@ -51,9 +52,9 @@ def get_args():
     parser.add_argument('--gw', required=True, help='Uplink network gateway', dest='uplink_gw')
     parser.add_argument('--ip', required=True, help='Uplink interface IP', dest='uplink_ip')
     parser.add_argument('--mac', required=True, help='Uplink interface MAC', dest='uplink_mac')
-    parser.add_argument('--vrs', required=True, help='VRS Gateway IP address', dest='vrs_ip')
-    parser.add_argument('--port', required=True, help='VRS Gateway Network Interface Name', dest='vrs_port')
-    parser.add_argument('--vlan', required=True, help='VRS Gateway Network Interface Vlan ID', dest='vrs_vlan')
+    parser.add_argument('--vsg', required=True, help='VSG/VRS-G name as it appears in your infrastructure (defaults to IP if you have not changed it)', dest='gw_name')
+    parser.add_argument('--port', required=True, help='VSG/VRS-G Network Interface Name', dest='gw_port')
+    parser.add_argument('--vlan', required=True, help='VSG/VRS-G Network Interface Vlan ID', dest='gw_vlan')
     args = parser.parse_args()
     return args
 
@@ -84,9 +85,9 @@ def main():
     uplink_gw           = args.uplink_gw
     uplink_ip           = args.uplink_ip
     uplink_mac          = args.uplink_mac
-    vrs_ip              = args.vrs_ip
-    vrs_port            = args.vrs_port
-    vrs_vlan            = args.vrs_vlan
+    gw_name              = args.gw_name
+    gw_port            = args.gw_port
+    gw_vlan            = args.gw_vlan
 
     # Logging settings
     if debug:
@@ -122,23 +123,55 @@ def main():
 
     nuage_user = nc.user
 
+
+    # Getting the parentID of FIP subnet
     logger.debug('Getting FIP subnet parent ID')
     fip_obj = nuage_user.subnets.get_first(filter="address == '{0}'".format(fip_net))
+   
+    # Fail if FIP subnet object was not found
+    if not fip_obj:
+        logger.critical('FIP subnet {0} was not found'.format(fip_net))
+        return 1
+
     shared_resource_id = fip_obj.parent_id
     logger.debug('FIP parent ID is: {0}'.format(shared_resource_id))
 
+
+    # Locating a gateway port and creating a new VLAN
     logger.debug('Creating a new VLAN on Gateway port')
-    new_vlan = vsdk.NUVLAN(value=vrs_vlan)
-    gw = nuage_user.gateways.get_first(filter="name == '{0}'".format(vrs_ip))
-    port = gw.ports.get_first(filter="name == '{0}'".format(vrs_port))
+    new_vlan = vsdk.NUVLAN(value=gw_vlan)
+    gw = nuage_user.gateways.get_first(filter="name == '{0}'".format(gw_name))
+
+    # Fail if Gateway was not found
+    if not gw:
+        logger.critical('Gateway {0} was not found'.format(gw_name))
+        return 1
+
+    port = gw.ports.get_first(filter="name == '{0}'".format(gw_port))
+
+    # Fail if Port requirements are not met
+    if not port:
+        logger.critical('Port {0} was not found on Gateway {1}'.format(gw_port, gw_name))
+        return 1
+    elif not port.port_type == 'ACCESS':
+        logger.critical('Port {0} is not an ACCESS port type'.format(gw_port))
+        return 1
+    elif not int(gw_vlan) in range(*[int(x) for x in port.vlan_range.split('-')]):
+        logger.critical('Vlan {0} is not part of the port vlan range: {1}'.format(gw_vlan, port.vlan_range))
+        return 1
+    elif port.vlans.get_first(filter="value == {0}".format(gw_vlan)):
+        logger.critical('Vlan {0} already exists on port {1}'.format(gw_vlan, gw_port))
+        return 1
+
     port.create_child(new_vlan)
     vlan_id = new_vlan.id
     logger.debug('New VLAN ID is: {0}'.format(vlan_id))
 
-    logger.debug('Creating an Uplink Subnet')
 
+    # Constructing an Uplink Subnet object
+    logger.debug('Creating an Uplink Subnet')
     shared_subnet = vsdk.NUSharedNetworkResource(name='uplink subnet {0}'.format(uplink_addr.replace('.','-')), \
-        description='Uplink subnet to Gateway {0}'.format(vrs_ip.replace('.','-')), \
+        description='Uplink subnet to Gateway {0}'.format(gw_name.replace('.','-')), \
         address=uplink_addr, \
         netmask=uplink_mask, \
         gateway=uplink_gw, \
@@ -147,10 +180,12 @@ def main():
         uplink_interface_mac=uplink_mac, \
         uplink_gw_vlan_attachment_id=vlan_id, \
         shared_resource_parent_id=shared_resource_id, \
-        uplink_vport_name = 'uplink vport {0} Vlan{1}'.format(vrs_port, vrs_vlan))
+        uplink_vport_name = 'uplink vport {0} Vlan{1}'.format(gw_port, gw_vlan))
 
+    # Creating a subnet on VSD
     nuage_user.create_child(shared_subnet)
-    logger.warning('Uplink Subnet is created')
+
+    logger.info('Uplink Subnet is created')
     return 0
 
 # Start program
